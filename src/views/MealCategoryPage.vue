9e23c79de47a307598bea52046f98ca97614c650
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-23 21:20:47 +0100

    List meal categories

--- src/views/MealCategoryPage.vue
+++ src/views/MealCategoryPage.vue
@@ -1,87 +1,73 @@
 <template>
-    <SideNavBarPage>
-        <v-container fluid class="mt-2">
-            <BreadCrumb page="Meal Category" />
-            <v-row>
-                <v-col md="12">
-                    <v-btn
-                        type="button"
-                        color="success"
-                        data-toggle="modal"
-                        data-target="#mealCategory"
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12">
+                <BreadCrumb />
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col cols="12">
+                <v-btn type="button" color="success">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-plus"
+                        viewBox="0 0 16 16"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                            class="bi bi-plus"
-                            viewBox="0 0 16 16"
-                        >
-                            <path
-                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                            />
-                        </svg>
-                        {{ $t("Add") }}
-                    </v-btn>
-                    <MealCategoryModal
-                        :mealCategory="mealCategoryClicked"
-                        @save="loadMealCategory()"
-                        @close="resetMealCategoryClicked()"
-                    />
-                </v-col>
-            </v-row>
-            <v-row class="mt-2">
-                <v-col
-                    md="4"
-                    v-for="mealCategory in mealCategories"
-                    :key="mealCategory.id"
-                >
-                    <MealCategoryObject :mealCategory="mealCategory" @click="() => setMealCategoryClicked(mealCategory)" @delete="loadMealCategory()" />
-                </v-col>
-            </v-row>
-        </v-container>
-    </SideNavBarPage>
+                        <path
+                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                        />
+                    </svg>
+                    {{ $t("Add") }}
+                </v-btn>
+            </v-col>
+        </v-row>
+
+        <v-row class="mt-2">
+            <v-col
+                md="4"
+                v-for="mealCategory in mealCategories"
+                :key="mealCategory.id"
+            >
+                <MealCategoryObject
+                    :mealCategory="mealCategory"
+                    @delete="loadMealCategory()"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-import { ref } from "vue";
-import SideNavBarPage from "../components/SideNavBarPage.vue";
 import BreadCrumb from "../components/BreadCrumb";
-import MealCategoryObject from "../components/MealCategoryObject";
 import MealCategoryEndpoints from "../axios/api/mealCategory";
-import MealCategoryModal from "../components/MealCategoryModal.vue";
-import { onMounted } from "vue";
+import MealCategoryObject from "../components/MealCategoryObject.vue";
 
 export default {
     components: {
-        SideNavBarPage,
         BreadCrumb,
         MealCategoryObject,
-        MealCategoryModal,
     },
 
-    setup() {
-        const mealCategoryClicked = ref(null);
-        const { load, mealCategory } = MealCategoryEndpoints.list();
-        const loadMealCategory = async () => {
-            await load();
-        };
-
-        const setMealCategoryClicked = (mealCategoryObject) => mealCategoryClicked.value = mealCategoryObject;
-        const resetMealCategoryClicked = () => {
-            mealCategoryClicked.value = null;
-        };
-
-        onMounted(loadMealCategory);
-
+    data() {
         return {
-            mealCategories: mealCategory,
-            loadMealCategory,
-            setMealCategoryClicked,
-            mealCategoryClicked,
-            resetMealCategoryClicked,
+            mealCategories: [],
         };
+    },
+
+    mounted() {
+        this.loadMealCategory();
+    },
+
+    methods: {
+        loadMealCategory() {
+            MealCategoryEndpoints.list().then(
+                ({ data }) => (this.mealCategories = data)
+            );
+        },
     },
 };
 </script>
