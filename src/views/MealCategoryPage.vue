47096adb875e600e692490cfc2a1cab4dce7abdb
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-23 21:37:05 +0100

    Display dialog

--- src/views/MealCategoryPage.vue
+++ src/views/MealCategoryPage.vue
@@ -8,21 +8,31 @@
 
         <v-row>
             <v-col cols="12">
-                <v-btn type="button" color="success">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        class="bi bi-plus"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                        />
-                    </svg>
-                    {{ $t("Add") }}
-                </v-btn>
+                <v-dialog v-model="showCreateDialog">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                            type="button"
+                            color="success"
+                            v-on="on"
+                            v-bind="attrs"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                class="bi bi-plus"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                                />
+                            </svg>
+                            {{ $t("Add") }}
+                        </v-btn>
+                    </template>
+                    <MealCategoryDialog />
+                </v-dialog>
             </v-col>
         </v-row>
 
@@ -32,10 +42,17 @@
                 v-for="mealCategory in mealCategories"
                 :key="mealCategory.id"
             >
-                <MealCategoryObject
-                    :mealCategory="mealCategory"
-                    @delete="loadMealCategory()"
-                />
+                <v-dialog v-model="showUpdateDialog">
+                    <template v-slot:activator="{ on, attrs }">
+                        <MealCategoryObject
+                            v-on="on"
+                            v-bind="attrs"
+                            :mealCategory="mealCategory"
+                            @delete="loadMealCategory()"
+                        />
+                    </template>
+                    <MealCategoryDialog :mealCategory="mealCategory" />
+                </v-dialog>
             </v-col>
         </v-row>
     </v-container>
@@ -44,17 +61,21 @@
 <script>
 import BreadCrumb from "../components/BreadCrumb";
 import MealCategoryEndpoints from "../axios/api/mealCategory";
-import MealCategoryObject from "../components/MealCategoryObject.vue";
+import MealCategoryObject from "../components/MealCategoryObject";
+import MealCategoryDialog from "../components/MealCategoryDialog";
 
 export default {
     components: {
         BreadCrumb,
         MealCategoryObject,
+        MealCategoryDialog,
     },
 
     data() {
         return {
             mealCategories: [],
+            showUpdateDialog: false,
+            showCreateDialog: false,
         };
     },
 
