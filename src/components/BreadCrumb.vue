9a88f8835d9f3803588faa1d46edea446b5f5b35
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-23 19:54:40 +0100

    Get breadcrumb items from metadata

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row>
+        <v-col md="12">
+            <v-breadcrumbs :items="getItems" />
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    computed: {
+        getItems() {
+            return this.$route.meta.breadcrumb;
+        },
+    },
+};
+</script>
