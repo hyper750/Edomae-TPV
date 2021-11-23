5773f9c136cc7dcf3b8d5c476a1b1d8ab43c460e
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-23 19:54:32 +0100

    Home example

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,19 @@
 <template>
-    <v-container fluid> Home </v-container>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="12">
+                <BreadCrumb />
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<script>
+import BreadCrumb from "../components/BreadCrumb.vue";
+
+export default {
+    components: {
+        BreadCrumb,
+    }
+};
+</script>
