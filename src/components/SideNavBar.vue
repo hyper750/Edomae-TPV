1f89de58087a46e0b2f68c705cda19538e15469f
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-23 20:47:25 +0100

    Stylize logout

--- src/components/SideNavBar.vue
+++ src/components/SideNavBar.vue
@@ -32,11 +32,13 @@
                     v-if="page.subgroup"
                     :value="false"
                     :prepend-icon="page.icon"
-                    style="width: 100%;"
+                    style="width: 100%"
                 >
                     <template v-slot:activator>
                         <v-list-item-content>
-                            <v-list-item-title>{{ $t(page.name) }}</v-list-item-title>
+                            <v-list-item-title>{{
+                                $t(page.name)
+                            }}</v-list-item-title>
                         </v-list-item-content>
                     </template>
 
@@ -50,32 +52,43 @@
                             <v-icon>{{ subpage.icon }}</v-icon>
                         </v-list-item-icon>
                         <v-list-item-content>
-                            <v-list-item-title>{{ $t(subpage.name) }}</v-list-item-title>
+                            <v-list-item-title>{{
+                                $t(subpage.name)
+                            }}</v-list-item-title>
                         </v-list-item-content>
                     </v-list-item>
                 </v-list-group>
             </v-list-item>
         </v-list>
-        
+
         <v-container fluid class="userMenu">
             <v-row>
                 <v-col cols="12">
                     <v-menu offset-y>
                         <template v-slot:activator="{ on, attrs }">
-                            <v-btn color="primary" dark v-bind="attrs" v-on="on">
-                                {{ username }}
-                            </v-btn>
+                            <v-container fluid v-bind="attrs" v-on="on" class="cursor-pointer">
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-list-item-avatar>
+                                            <v-img
+                                                src="@/assets/avatar-icon.png"
+                                            ></v-img>
+                                        </v-list-item-avatar>
+                                        <span class="font-weight-bold text--secondary text-capitalize">{{ username }}</span>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
                         </template>
 
                         <v-list>
                             <v-list-item link @click="logout">
                                 <v-list-item-icon>
-                                    <v-icon>
-                                        mdi-logout
-                                    </v-icon>
+                                    <v-icon> mdi-logout </v-icon>
                                 </v-list-item-icon>
                                 <v-list-item-content>
-                                    <v-list-item-title>{{ $t('Logout') }}</v-list-item-title>
+                                    <v-list-item-title>{{
+                                        $t("Logout")
+                                    }}</v-list-item-title>
                                 </v-list-item-content>
                             </v-list-item>
                         </v-list>
@@ -119,10 +132,10 @@ export default {
                             icon: "mdi-shape",
                         },
                         {
-                            name: 'Meal',
-                            link: '/meal',
-                            icon: 'mdi-food-fork-drink'
-                        }
+                            name: "Meal",
+                            link: "/meal",
+                            icon: "mdi-food-fork-drink",
+                        },
                     ],
                 },
             ],
@@ -132,12 +145,12 @@ export default {
     computed: {
         getPageList() {
             return this.pageList.filter((page) => {
-                if( !page.permissions ) {
+                if (!page.permissions) {
                     return true;
                 }
 
                 let permission = true;
-                for(let permissionFunc of page.permissions) {
+                for (let permissionFunc of page.permissions) {
                     permission &&= permissionFunc();
                 }
 
