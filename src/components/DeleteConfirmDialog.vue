d2ee084f3f8fd9c4426ac00d471eaf4fc717322d
Raul <raulmarquespalmer_150@hotmail.com>
2021-11-25 17:46:42 +0100

    Added delete dialog

---
+++ src/components/DeleteConfirmDialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card>
+        <v-card-title>
+            {{ $t("Are you sure you want to delete it?") }}
+        </v-card-title>
+
+        <v-card-actions>
+            <v-spacer/>
+            <v-btn color="blue darken-1" text @click="() => deny()">
+                {{ $t('Close') }}
+            </v-btn>
+            <v-btn color="error darken-1" text @click="() => accept()">
+                {{ $t('Delete') }}
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    methods: {
+        accept() {
+            this.$emit("accept");
+        },
+
+        deny() {
+            this.$emit("deny");
+        },
+    },
+};
+</script>
