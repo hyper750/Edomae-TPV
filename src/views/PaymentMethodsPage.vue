edfa3792a7c8321240c314c8e24bc2f44aeb2926
Raul <raulmarquespalmer_150@hotmail.com>
2021-12-04 22:26:17 +0100

    Create add button

--- src/views/PaymentMethodsPage.vue
+++ src/views/PaymentMethodsPage.vue
@@ -5,15 +5,77 @@
                 <BreadCrumb />
             </v-col>
         </v-row>
+
+        <v-row>
+            <v-col cols="12">
+                <v-dialog v-model="showCreateDialog">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn color="success" v-on="on" v-bind="attrs">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="currentColor"
+                                class="bi bi-plus"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                                />
+                            </svg>
+                            {{ $t("Add") }}
+                        </v-btn>
+                    </template>
+                    <PaymentMethodDialog
+                        @close="closeCreateDialog"
+                        @save="saveCreateDialog"
+                    />
+                </v-dialog>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
 import BreadCrumb from "../components/BreadCrumb.vue";
+import PaymentMethodsEndpoints from "../axios/api/paymentMethods";
 
 export default {
     components: {
         BreadCrumb,
+    },
+
+    data() {
+        return {
+            showCreateDialog: false,
+            paymentMethodsAvailable: [],
+        };
+    },
+
+    mounted() {
+        this.loadPaymentMethods();
+    },
+
+    methods: {
+        loadPaymentMethods() {
+            PaymentMethodsEndpoints.list()
+                .then(({ data }) => (this.paymentMethodsAvailable = data))
+                .catch(() =>
+                    this.$store.dispatch(
+                        "setGlobalError",
+                        this.$i18n.t("Can't list payment methods")
+                    )
+                );
+        },
+
+        closeCreateDialog() {
+            this.showCreateDialog = false;
+        },
+
+        saveCreateDialog() {
+            this.closeCreateDialog();
+            this.loadPaymentMethods();
+        },
     },
 };
 </script>
