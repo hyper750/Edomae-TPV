416b513e25e6a3d002df133f312cd3ee09d5b834
Raul <raulmarquespalmer_150@hotmail.com>
2021-12-09 18:58:01 +0100

    Added local endpoints

---
+++ src/axios/api/local.js
@@ -0,0 +1,42 @@
+import { getApiWithLogin } from "../axios";
+
+export default {
+    ENDPOINT: '/local',
+
+    async list(filters) {
+        return await getApiWithLogin().get(this.ENDPOINT, filters);
+    },
+
+    async post(local) {
+        const { enabled, name, imatge } = local;
+
+        const formData = new FormData();
+        formData.append('enabled', enabled);
+        formData.append('name', name);
+        formData.append('imatge', imatge, imatge.name);
+
+        return await getApiWithLogin().post(this.ENDPOINT, formData);
+    },
+
+    async put(id, local) {
+        const { enabled, name, imatge } = local;
+
+        // TODO: Why is obligated to update the imatge?
+        const formData = new FormData();
+        formData.append('enabled', enabled);
+        formData.append('name', name);
+        if (imatge) {
+            formData.append('imatge', imatge, imatge.name);
+        }
+
+        return await getApiWithLogin().put(`${this.ENDPOINT}/${id}`, formData);
+    },
+
+    async get(id) {
+        return await getApiWithLogin().get(`${this.ENDPOINT}/${id}`);
+    },
+
+    async delete(id) {
+        return await getApiWithLogin().delete(`${this.ENDPOINT}/${id}`);
+    },
+};
