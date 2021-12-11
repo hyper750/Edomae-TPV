414775a7fbd407ded95e3a0798bc4926bc7cefa6
Raul <raulmarquespalmer_150@hotmail.com>
2021-12-11 15:56:36 +0100

    Select the local to move the tables

--- src/views/TablePage.vue
+++ src/views/TablePage.vue
@@ -5,15 +5,70 @@
                 <BreadCrumb />
             </v-col>
         </v-row>
+
+        <v-row>
+            <v-col md="3">
+                <v-select
+                    :items="getItems"
+                    v-model="selectedLocal"
+                    :label="$t('Select the local')"
+                />
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
 import BreadCrumb from "../components/BreadCrumb";
+import LocalEndpoints from "../axios/api/local";
 
 export default {
     components: {
         BreadCrumb,
+    },
+
+    mounted() {
+        this.loadLocals();
+    },
+
+    data() {
+        return {
+            locals: [],
+            selectedLocal: null,
+        };
+    },
+
+    computed: {
+        getItems() {
+            return this.locals.map((local) => {
+                let localName = local.name;
+                if (!local.enabled) {
+                    localName = `${local.name} (${this.$i18n.t("Disabled")})`;
+                }
+                return {
+                    text: localName,
+                    value: local.id,
+                };
+            });
+        },
+    },
+
+    methods: {
+        loadLocals() {
+            this.selectedLocal = null;
+
+            LocalEndpoints.list()
+                .then(({ data }) => {
+                    this.locals = data;
+                    this.selectedLocal = data[0].id;
+                })
+                .catch(() =>
+                    this.$store.dispatch(
+                        "setGlobalError",
+                        this.$i18n.t("Can't load locals")
+                    )
+                );
+        },
     },
 };
 </script>
