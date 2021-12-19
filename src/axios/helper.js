af6463d8b05f15a93df0142545c546ae52dc7c87
Raul <raulmarquespalmer_150@hotmail.com>
2021-12-19 21:18:12 +0100

    Added axios helper

---
+++ src/axios/helper.js
@@ -0,0 +1,21 @@
+export default {
+    removeOptionalValues(formData) {
+        const result = new FormData();
+        
+        // Null, undefined... values are serialized to string
+        const blackListValues = [
+            'undefined',
+            'null'
+        ];
+
+        for(const [key, value] of formData) {
+            // Avoid to include null values
+            if(blackListValues.includes(value)) {
+                continue;
+            }
+            result.set(key, value);
+        }
+
+        return result;
+    },
+};
