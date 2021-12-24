97932d57464828fdc7495c1c965b5780c09c3afc
Raul <raulmarquespalmer_150@hotmail.com>
2021-12-24 21:00:16 +0100

    Added command meal endpoint

---
+++ src/axios/api/commandMeal.js
@@ -0,0 +1,47 @@
+import { getApiWithLogin } from "../axios";
+import AxiosHelper from "../helper";
+
+
+export default {
+    ENDPOINT: '/commandMeal',
+
+    async list(filters) {
+        return await getApiWithLogin().get(this.ENDPOINT, { params: filters });
+    },
+
+    async get(id) {
+        return await getApiWithLogin().get(`${this.ENDPOINT}/${id}`);
+    },
+
+    async post(commandMeal) {
+        const { command, meal, number, extra, discount } = commandMeal;
+
+        let formData = new FormData();
+        formData.append('command', command);
+        formData.append('meal', meal);
+        formData.append('number', number);
+        formData.append('extra', extra);
+        formData.append('discount', discount);
+        formData = AxiosHelper.removeOptionalValues(formData);
+
+        return await getApiWithLogin().post(this.ENDPOINT, formData);
+    },
+
+    async put(id, commandMeal) {
+        const { command, meal, number, extra, discount } = commandMeal;
+
+        const formData = new FormData();
+        formData.append('command', command);
+        formData.append('meal', meal);
+        formData.append('number', number);
+        formData.append('extra', extra);
+        formData.append('discount', discount);
+        formData = AxiosHelper.removeOptionalValues(formData);
+
+        return await getApiWithLogin().put(`${this.ENDPOINT}/${id}`, formData);
+    },
+
+    async delete(id) {
+        return await getApiWithLogin().delete(`${this.ENDPOINT}/${id}`);
+    },
+};
