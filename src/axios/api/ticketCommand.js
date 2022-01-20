045f6a3278c0d5bdb07e30f06b7471cdc71133ec
Raul <raulmarquespalmer_150@hotmail.com>
2022-01-20 07:07:20 +0100

    Added ticket list api

--- src/axios/api/ticketCommand.js
+++ src/axios/api/ticketCommand.js
@@ -3,6 +3,10 @@ import { getApiWithLogin } from "../axios";
 export default {
     ENDPOINT: '/ticket/command',
 
+    async list() {
+        return await getApiWithLogin().get(this.ENDPOINT);
+    },
+
     async get(id) {
         return await getApiWithLogin().get(`${this.ENDPOINT}/${id}`);
     },
