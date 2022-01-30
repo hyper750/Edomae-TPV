c14dcdbeabd37aa5cd5292432cd1873acc1d1b11
Raul <raulmarquespalmer_150@hotmail.com>
2022-01-30 22:06:50 +0100

    Replace svg for v-icon

--- src/views/LocalPage.vue
+++ src/views/LocalPage.vue
@@ -11,18 +11,9 @@
                 <v-dialog v-model="showCreateDialog" @click:outside="resetCreateLocalDialog">
                     <template v-slot:activator="{ on, attrs }">
                         <v-btn color="success" v-on="on" v-bind="attrs">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                fill="currentColor"
-                                class="bi bi-plus"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-                                />
-                            </svg>
+                            <v-icon dense class="mr-1">
+                                mdi-plus
+                            </v-icon>
                             {{ $t("Add") }}
                         </v-btn>
                     </template>
